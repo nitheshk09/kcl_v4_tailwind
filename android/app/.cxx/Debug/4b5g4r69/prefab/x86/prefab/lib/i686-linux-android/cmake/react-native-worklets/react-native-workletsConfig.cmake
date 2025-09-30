ae4564a4d865c0455b639582efcff6b0fd648541
Naga Sainath Reddy <sainath.palle@empover.com>
2025-09-30 15:34:18 +0530

    tuesday changes

---
+++ android/app/.cxx/Debug/4b5g4r69/prefab/x86/prefab/lib/i686-linux-android/cmake/react-native-worklets/react-native-workletsConfig.cmake
@@ -0,0 +1,9 @@
+if(NOT TARGET react-native-worklets::worklets)
+add_library(react-native-worklets::worklets SHARED IMPORTED)
+set_target_properties(react-native-worklets::worklets PROPERTIES
+    IMPORTED_LOCATION "/Users/nagasainathreddy/kcl_fork/kcl_v4_tailwind/node_modules/react-native-worklets/android/build/intermediates/cxx/Debug/4p64311s/obj/x86/libworklets.so"
+    INTERFACE_INCLUDE_DIRECTORIES "/Users/nagasainathreddy/kcl_fork/kcl_v4_tailwind/node_modules/react-native-worklets/android/build/prefab-headers/worklets"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
